76b9c4418b91bfa8d9c03e3ea73435721c362a76
neerajmasai <neeraj_fw10_110@masai.school>
2022-01-11 12:04:00 +0530

    project structure and template added

---
+++ server.js
@@ -0,0 +1,26 @@
+//config data
+const {DEFAULT_CONNECTION_STRING, PORT, MONGOOSE_OPTIONS} = require("./src/config/config");
+
+//connect to express
+const express = require("express");
+const app = express();
+app.listen(PORT, () => console.log(`DSA Tracker Web App connected successfully to Express. Listening on port ${PORT}...`));
+
+//middlewares
+const cors = require("cors");
+app.use(cors())
+app.use(express.json());
+
+//connect to mongoose
+const mongoose = require('mongoose');
+mongoose.connect(DEFAULT_CONNECTION_STRING, MONGOOSE_OPTIONS);
+mongoose.connection.on("error", err => {
+  console.log("Connection Error. DSA Tracker Web App could not successfully connect to Mongoose.", err);
+});
+mongoose.connection.on("connected", (err, res) => {
+    console.log("DSA Tracker Web App connected successfully to Mongoose.");
+});
+
+//sample routes 
+const dummyController = require("./src/controllers/dummy.controller.js");
+app.use("/dummy", dummyController);
---
+++ src/config/config.js
@@ -0,0 +1,28 @@
+// GLOBAL CONFIG ENVIRONMENT VARIABLES
+
+
+//server host name
+const HOST = "localhost";
+
+//remote database name
+const DATABASE = "DSATracker";
+
+//database credentials
+const USERNAME = "dsatracker";
+const PASSWORD = "dsatracker";
+
+//default app port
+const PORT = 2345;
+
+//default connection url
+const DEFAULT_CONNECTION_STRING = `mongodb+srv://${USERNAME}:${PASSWORD}@dsatracker.1osbv.mongodb.net/${DATABASE}?retryWrites=true&w=majority`; 
+
+//mongoose options for connection
+const MONGOOSE_OPTIONS = {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology:true,
+    useNewUrlParser:true
+}
+
+module.exports = {HOST, PORT, DATABASE, USERNAME, PASSWORD, DEFAULT_CONNECTION_STRING, MONGOOSE_OPTIONS}
